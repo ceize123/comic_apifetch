9d3d12b6a73749f7696d45000b01edab0d3bbfe2
ytkuo <hahaeating1@gmail.com>
2021-10-13 11:47:49 -0400

    test

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
-const express = require("express");
-const path = require("path");
-const cors = require('cors');
+var express = require("express");
+var path = require("path");
+var cors = require('cors');
 
-const app = express();
+var app = express();
 
-const HTTP_PORT = process.env.PORT || 8080;
+var HTTP_PORT = process.env.PORT || 8080;
 
 // The server must output: "Express http server listening on port" - to the console, 
 // where port is the port the server is currently listening on
@@ -13,25 +13,24 @@ function onHttpStart() {
 }
 
 app.use(express.static('public'));
-const corsOptions ={
-    origin:'https://tranquil-peak-64703.herokuapp.com/', 
-    credentials:true,            //access-control-allow-credentials:true
-    optionSuccessStatus:200
-}
-app.use(cors(corsOptions));
-// app.use(cors()); // Use this after the variable declaration
+
+// const corsOptions ={
+//     origin:'http://localhost:8080/', 
+//     credentials:true,            //access-control-allow-credentials:true
+//     optionSuccessStatus:200
+// }
+// app.use(cors(corsOptions));
+app.use(cors()); // Use this after the variable declaration
+// app.use(cors({origin: '*'}));
+// app.options("/", cors(corsOptions));  
+
 
 app.get("/", function (req, res) {
-    res.header("Access-Control-Allow-Origin", "*");
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE'); // If needed
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type'); // If needed
+    res.setHeader('Access-Control-Allow-Credentials', true); // If needed
     res.sendFile(path.join(__dirname, "/views/home.html")); 
 });
-
-// app.get("/api/users", (req, res) => {
-//     res.json({message: "fetch all users"});
-// });
-
-// app.get("/api/users/:userId", (req, res) => {
-//     res.json({message: "get user with Id: " + req.params.userId});
-// });
 
 app.listen(HTTP_PORT, onHttpStart);
