34359d237787ba9a96437a19172f37b44b531ac1
ytkuo <hahaeating1@gmail.com>
2021-10-13 10:39:08 -0400

    test

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const path = require("path");
+const cors = require('cors');
+
+const app = express();
+
+const HTTP_PORT = process.env.PORT || 8080;
+
+// The server must output: "Express http server listening on port" - to the console, 
+// where port is the port the server is currently listening on
+function onHttpStart() {
+    console.log("Express http server listening on: " + HTTP_PORT);
+}
+
+app.use(express.static('public'));
+app.use(cors()); // Use this after the variable declaration
+
+app.get("/", function (req, res) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.sendFile(path.join(__dirname, "/views/home.html")); 
+});
+
+// app.get("/api/users", (req, res) => {
+//     res.json({message: "fetch all users"});
+// });
+
+// app.get("/api/users/:userId", (req, res) => {
+//     res.json({message: "get user with Id: " + req.params.userId});
+// });
+
+app.listen(HTTP_PORT, onHttpStart);
