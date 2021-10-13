eedf6aefe7ee617f2a7ce94118d82f1f09fbb873
ytkuo <hahaeating1@gmail.com>
2021-10-13 10:42:22 -0400

    test

--- server.js
+++ server.js
@@ -13,7 +13,14 @@ function onHttpStart() {
 }
 
 app.use(express.static('public'));
-app.use(cors()); // Use this after the variable declaration
+const cors = require('cors');
+const corsOptions ={
+    origin:'https://tranquil-peak-64703.herokuapp.com/', 
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200
+}
+app.use(cors(corsOptions));
+// app.use(cors()); // Use this after the variable declaration
 
 app.get("/", function (req, res) {
     res.header("Access-Control-Allow-Origin", "*");
