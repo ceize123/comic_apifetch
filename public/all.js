81a244f3d2258c73508b9af8e76dcbc0169981f9
ytkuo <hahaeating1@gmail.com>
2021-10-14 21:59:21 -0400

    10/14 done

---
+++ public/all.js
@@ -0,0 +1,126 @@
+let max = Infinity;
+let minute = document.querySelector(".minutes");
+let second = document.querySelector(".seconds");
+
+const loadingRequest = method => {
+    let path = window.location.pathname.replace('/', '');
+    let comicNum = "";
+    if (path !== '') {
+        comicNum = path;
+    }
+    fetch(`/api/${comicNum}`, {
+        method: method,
+        headers: { 'Content-Type': 'application/json' },
+        mode: "no-cors",
+    })
+    .then((response) => {
+        return response.json();
+    })
+    .then((response) => {
+        max = response.num; 
+        document.getElementById('num').textContent = response.num;
+        document.getElementById('time').textContent = `${response.month}-${response.day}-${response.year}`;
+        document.getElementById('time').setAttribute("datetime",`${response.month}-${response.day}-${response.year}`);
+        document.getElementById('title').textContent = response.title;
+        let transcript = response.transcript.replace(/(?:\r\n|\r|\n)/g, '<br>');
+        document.getElementById('transcript').innerHTML = transcript;
+        console.log(response.transcript);
+        document.getElementById('comic').src = response.img;
+    });
+};
+
+const makeAJAXRequest = (method, comicNum) => {
+    // if ((comicNum > max || comicNum <= 1) && comicNum !== "") {
+    //     alert("Hi");
+    //     return;
+    // }
+
+    // console.log(max);
+    // console.log(window.location.pathname);
+    // let path = window.location.pathname.replace('/', '');
+    
+    // if (path !== '') {
+    //     comicNum = path;
+    // }
+    // console.log(path);
+    // console.log(comicNum);
+
+    fetch(`/api/${comicNum}`, {
+        method: method,
+        headers: { 'Content-Type': 'application/json' },
+        mode: "no-cors",
+    })
+    .then((response) => {
+        // window.location = url;
+        return response.json();
+    })
+    .then((response) => {
+        if (response.num) {
+            document.getElementById('num').textContent = response.num;
+            document.getElementById('time').textContent = `${response.month}-${response.day}-${response.year}`;
+            document.getElementById('time').setAttribute("datetime",`${response.month}-${response.day}-${response.year}`);
+            document.getElementById('title').textContent = response.title;
+            let transcript = response.transcript.replace(/(?:\r\n|\r|\n)/g, '<br>');
+            document.getElementById('transcript').innerHTML = transcript;
+            document.getElementById('comic').src = response.img;
+            history.pushState({}, null, `${comicNum}`);
+        } else {
+            swal({
+                title: "The comic number does not exist.",
+                icon: "warning",
+                dangerMode: true,
+            });
+        }
+    });
+};
+
+const nextComic = (num) => {
+    resetTime();
+    makeAJAXRequest("GET", parseInt(num.textContent) + 1);
+    
+};
+
+const previousComic = (num) => {
+    resetTime();
+    makeAJAXRequest("GET", parseInt(num.textContent) - 1);
+};
+
+const random = () => {
+    resetTime();
+    makeAJAXRequest("GET", Math.floor(Math.random() * max + 1));
+};
+
+
+
+const search = (select) => {
+    let num = document.getElementById('num');
+    switch (select) {
+        case 1:
+            nextComic(num);
+            break;
+        case -1:
+            previousComic(num);
+            break;
+        case 0:
+            random();
+            break;
+    }
+};
+
+const timePassed = () => {
+    if (parseInt(second.textContent) < 59)
+        second.textContent = ('0' + parseInt(++second.textContent)).slice(-2);
+    else if (parseInt(second.textContent) === 59) {
+        second.textContent = '00';
+        minute.textContent = parseInt(++minute.textContent);
+    }
+};
+
+let timeSet = setInterval(timePassed, 1000);
+
+const resetTime = () => {
+    clearInterval(timeSet);
+    second.textContent = '00';
+    minute.textContent = '00';
+    timeSet = setInterval(timePassed, 1000);
+}
--- server.js
+++ server.js
@@ -5,7 +5,6 @@ const exphbs = require("express-handlebars");
 const app = express();
 const HTTP_PORT = process.env.PORT || 8080;
 let data = {};
-let maximum = 0;
 
 // The server must output: "Express http server listening on port" - to the console, 
 // where port is the port the server is currently listening on
@@ -14,33 +13,49 @@ function onHttpStart() {
 }
 
 app.use(express.static('public'));
+// app.use('/:id', express.static('public'));
 app.engine('.hbs', exphbs({ extname: '.hbs' }));
 app.set('view engine', '.hbs');
 
-app.get('/', async (req, res) => {
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, "/public/index.html")); 
+});
+
+
+app.get('/api', async (req, res) => {
     let url = 'https://xkcd.com/info.0.json';
     data = await fetch(url)
-        .then((d) => d.json());
-    maximum = data.num;
-    // res.send(data);
-    res.render('viewData', {
-        dt: data,
-        layout: false
-    });
+    .then((d) => d.json());
+    res.send(data);
     // res.sendFile(path.join(__dirname, "/views/home.html")); 
+});
+
+app.get('/:id', async (req, res) => {
+    let url = `https://xkcd.com/${req.params.id}/info.0.json`;
+    let data = await fetch(url)
+        .then((d) => d.json())
+        .catch((err) => err);
+    if (data.num !== undefined) {
+        res.sendFile(path.join(__dirname, "/public/index.html")); 
+    } else {
+        res.redirect('/');
+    }
+    // res.redirect(`/${req.params.id}`);
+    // res.render('/public/index.html');
 
 });
 
-app.get("/:id", async (req, res) => {
+app.get("/api/:id", async (req, res) => {
     let url = `https://xkcd.com/${req.params.id}/info.0.json`;
     let data = await fetch(url)
         .then((d) => d.json())
     .catch((err) => err);
         
-    res.render('viewData', {
-        dt: data,
-        layout: false,
-    });
+    res.send(data);
+    // res.render('viewData', {
+    //     dt: data,
+    //     layout: false,
+    // });
 });
 
 // 404 route
